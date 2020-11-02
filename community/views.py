f3f67060aae6429292b18cc37f633d936ac1d31b
priscillapepe <priscillaungai99@gmail.com>
2020-11-02 16:18:30 +0300

    updated file

--- community/views.py
+++ community/views.py
@@ -1,29 +1,32 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
 # Create your views here.
+
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
 
 def signup(request):
-    form = UserCreationForm()
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        print('hello')
-        if form.is_valid():
-            
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+    if request.method == "POST":
 
-            account = Account(user=user)
-            account.save()
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
             login(request, user)
+
             return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
-        
     
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'signup.html', context)
+
+
