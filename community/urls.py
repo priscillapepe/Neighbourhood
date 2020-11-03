24b804d8efa3d6de2a4ec2786190f7d0f03f3dd5
priscillapepe <priscillaungai99@gmail.com>
2020-11-03 15:18:14 +0300

    modified urls.py

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
 from .views import home
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name='home'),
+    path('home/', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('index/', views.index, name='index'),
+    path('town/', views.town, name='town'),
+    path('mshomo/', views.index, name='mshomo'),
+    path('user/(?P<username>\)', views.profile, name='profile'),
+    path('upload/', views.upload_post, name='upload_post'),
+    path('accounts/edit/', views.edit_profile, name='edit_profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
